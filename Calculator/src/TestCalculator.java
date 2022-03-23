bd82f41b36e08b5d5826f756aec0a91e8f594957
RAGAPRIYA27 <101916826+RAGAPRIYA27@users.noreply.github.com>
2022-03-23 17:44:32 +0530

    Test Calculator

---
+++ Calculator/src/TestCalculator.java
@@ -0,0 +1,16 @@
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator=new Calculator();
+		System.out.println(calculator.add(10, 20));
+		System.out.println(calculator.sub(50, 40));
+		System.out.println(calculator.mul(50, 60));
+		System.out.println(calculator.div(70, 80));
+		System.out.println(calculator.square(10));
+		System.out.println(calculator.remainder(110, 120));
+		
+	}
+
+}
